8d892792c654ebf0ae014ee400cd34e33f0273d3
Rubenbze <rdonis83@gmail.com>
2026-05-13 02:05:39 -0600

    Major cinematic redesign

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import '../styles/Home.css'
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 function Home() {
   return (
@@ -7,28 +8,45 @@ function Home() {
 
       <section className="hero">
 
-        <div className="hero-content">
+        <motion.div
+          className="hero-content"
+
+          initial={{ opacity: 0, y: 60 }}
+
+          animate={{ opacity: 1, y: 0 }}
+
+          transition={{ duration: 1 }}
+        >
 
           <h1>
             Ruben's Gaming Garage
           </h1>
 
           <p>
-            Premium gaming setups,
-            next-gen gear,
+            Next-generation gaming setups,
             esports energy,
-            and immersive tech experiences.
+            anime culture,
+            and immersive technology.
           </p>
 
           <Link to="/games">
 
-            <button>
-              Explore The Garage
-            </button>
+            <motion.button
+
+              whileHover={{
+                scale: 1.08
+              }}
+
+              whileTap={{
+                scale: 0.95
+              }}
+            >
+              Enter The Garage
+            </motion.button>
 
           </Link>
 
-        </div>
+        </motion.div>
 
       </section>
 
